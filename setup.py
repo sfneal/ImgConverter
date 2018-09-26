e022e6086393e518ec3ff2ec28d67b24e6320144
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 16:08:22 -0400

    Updated to version 1.0.1 and fixed install_requires syntax error

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='ImgConverter',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
-    install_requies=[
+    install_requires=[
         'psdconvert',
         'pdfconduit-convert'
     ],
