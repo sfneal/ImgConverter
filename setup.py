778ec0f3f896504e6202fbfded3f70465e8e69d9
mrstephenneal <stephen.neal14@gmail.com>
2018-09-26 16:34:24 -0400

    Updated PyPi distribution 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ImgConverter',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     install_requires=[
         'psdconvert>=0.1.5',
