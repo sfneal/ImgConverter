467d425bf2056fa033175c3757e4328ad9cb226d
mrstephenneal <stephen.neal14@gmail.com>
2018-09-27 12:28:25 -0400

    Added version number retrieval

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import re
 from setuptools import setup, find_packages
+
+# Retrieve version number
+VERSIONFILE = "ImgConverter/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
 
 setup(
     name='ImgConverter',
-    version='1.0.3',
+    version=verstr,
     packages=find_packages(),
     install_requires=[
         'psdconvert>=0.1.5',
