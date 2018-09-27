73c67482b840d82bc91e303797b0c640b132cd01
mrstephenneal <stephen.neal14@gmail.com>
2018-09-27 16:45:30 -0400

    Modified dummy image size

--- ImgConverter/dummy.py
+++ ImgConverter/dummy.py
@@ -5,13 +5,13 @@ from PIL import Image, ImageDraw
 
 def dummy_img(source, target):
     astr = "ImgConverter error: unsupported file type\n" + os.path.basename(source)
-    para = textwrap.wrap(astr, width=15)
+    para = textwrap.wrap(astr, width=50)
 
-    MAX_W, MAX_H = 400, 600
+    MAX_W, MAX_H = 400, 200
     im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
     draw = ImageDraw.Draw(im)
 
-    current_h, pad = 50, 10
+    current_h, pad = 100, 10
     for line in para:
         w, h = draw.textsize(line)
         draw.text(((MAX_W - w) / 2, current_h), line)
